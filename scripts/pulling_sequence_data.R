d3c697634c59dbbdb883bb6cacc0cba4999fc895
Bailey <bailey.jannuzzi@gmail.com>
2025-09-27 23:05:32 -0600

    Dataset Activity Script and Files

--- scripts/pulling_sequence_data.R
+++ scripts/pulling_sequence_data.R
@@ -6,12 +6,27 @@ library(XML)
 library(httr)
 library(seqinr)
 source("./potential_functions.R")
+source("./please.R")
 
 #### Defining Variables: ####
+set_entrez_key("d9b97feea308d7dbc357193e79a85721f808")
+
+
 cich_search <- entrez_search(db = "taxonomy", term = "txid8113[SBTR]", retmax = 5000)
 search_ids <- cich_search$ids
 cichlids <- load_taxa() %>% dplyr::filter(Family == "Cichlidae") # list of all valid species from fishbase
 
+valid_tax <- create_ds_optimized(cich_search, search_ids, cichlids)
+
+# Get taxid for taxa of interest, not all species need to match every genes (we can make a super matrix). I think we should try to have two genes for every species we use, but that might not be possible.
+
+
+list_cols <- sapply(valid_tax, is.list)
+valid_tax[list_cols] <- lapply(valid_tax[list_cols], function(x) {
+  sapply(x, function(y) paste(y, collapse = "; "))
+})
+
+write.csv(valid_tax, "./data/raw/valid_taxa.csv", row.names = FALSE)
 
 #### Pulling data and making dataset ####
 # Potential genes of interest (only need 3 for the upcoming activity):
@@ -22,23 +37,52 @@ cichlids <- load_taxa() %>% dplyr::filter(Family == "Cichlidae") # list of all v
 
 
 # Finding accession numbers for a gene across at least 50 taxa
+seq_count <- 0
 
-for (i in 1:nrow(cich_stat_safe)) {
+for (i in 1:nrow(valid_tax)) {
   
-  name <- cich_stat_safe$species_name[i]
-  nuc_search <- entrez_search(db = "nuccore", 
-                              term = paste0(name, "[ORGN] AND COI[GENE]"), 
-                              retmax = 5)
+  if (seq_count >= 50) {
+    print("50 taxa hits for COI have been found.")
+    break
+  }
   
-  print(nuc_search$ids)
-  cich_stat_safe$accession_num[i] <- nuc_search$ids[1]
+  name <- valid_tax$species_name[i]
+  
+  tryCatch({
+    
+    nuc_search <- entrez_search(db = "nuccore", 
+                                term = paste0('"', name, '"', '[ORGN] AND COI[GENE]'), 
+                                retmax = 5)
+    
+    if(length(nuc_search$ids)<1) {
+      valid_tax[i,3:5] <- NA
+    }
+    
+    else {
+      sumout <- entrez_summary(db = "nuccore", id = nuc_search$ids[1])
+      valid_tax[i,3] <- sumout$caption
+      valid_tax[i,4] <- sumout$title
+      valid_tax[i,5] <- sumout$slen
+      
+      print(paste("Sequence found for", name, "Getting fasta"))
+      
+      seq.dat <- entrez_fetch(db = "nuccore", id = nuc_search$ids[1], rettype = "fasta")
+      coi_sequence <- c(coi_sequence, seq.dat)
+      
+      print("Fasta added.")
+      seq_count <- seq_count + 1
+    }
+    
+    })
 }
+print("DONE")
+
+writeLines(as.character(coi_sequence), "./data/raw/metadata/coi_out.fasta")
+
 
 # This table will contain all species that had hits for COI
-cich_coi_df <- cich_stat_safe
-
 # Creating a copy for saving
-cich_coi_save <- cich_coi_df
+cich_coi_save <- valid_tax
 
 # Convert list columns to character strings
 list_cols <- sapply(cich_coi_save, is.list)
@@ -46,44 +90,63 @@ cich_coi_save[list_cols] <- lapply(cich_coi_save[list_cols], function(x) {
   sapply(x, function(y) paste(y, collapse = "; "))
 })
 
-write.csv(cich_coi_save, "./Cichlid_COI.csv", row.names = FALSE)
+write.csv(cich_coi_save, "./data/raw/Cichlid_COI.csv", row.names = FALSE)
 
 
-cich_coi_df %>% 
-  filter(sapply(accession_num, length) != 0) %>% 
-  count()
-
-      # 400/1200 species had results for the COI gene
-
-
-
-# Now using that 400 species, let's do it again but for another gene
-# Using the saved table will help use filter even further down to less taxa (We can include more taxa later, I just think working with not 1200 species would be a lot)
-
-# NOTE: I know I wrote above that I was using the saved table, but I forgot to actually create a new table that only includes rows that actually had accession numbers, so we will have to do that at some point. But for now,
-        # I really don't care.
-
-        # A solution would be to just compare each table, and remove species that don't have accession numbers in both tables, and then compare that table to the third table. Hopefully that leaves us with more than 50 species
-        # so we can do more filter as we add another 2 genes.
+cich_coi_save %>% 
+  dplyr::filter(accession_num != 'NA') %>% 
+  summarise(count = n())
 
 
 
-#### Now let's look at the Ptr gene? ####
+#### Now let's look at the Ptr gene ####
 
-cich_ptr_df <- cich_coi_df
+seq_count <- 0
 
-for (i in 1:nrow(cich_ptr_df)) {
+for (i in 1:nrow(valid_tax)) {
   
-  name <- cich_ptr_df$species_name[i]
-  nuc_search <- entrez_search(db = "nuccore", 
-                              term = paste0(name, "[ORGN] AND Ptr gene"), 
-                              retmax = 5)
+  if (seq_count >= 50) {
+    print("50 taxa hits for Ptr have been found.")
+    break
+  }
   
-  print(nuc_search$ids)
-  cich_ptr_df$accession_num[i] <- nuc_search$ids[1]
+  name <- valid_tax$species_name[i]
+  
+  tryCatch({
+    
+    nuc_search <- entrez_search(db = "nuccore", 
+                                term = paste0('"', name, '"', '[ORGN] AND Ptr[GENE]'), 
+                                retmax = 5)
+    
+    if(length(nuc_search$ids)<1) {
+      valid_tax[i,3:5] <- NA
+    }
+    
+    else {
+      sumout <- entrez_summary(db = "nuccore", id = nuc_search$ids[1])
+      valid_tax[i,3] <- sumout$caption
+      valid_tax[i,4] <- sumout$title
+      valid_tax[i,5] <- sumout$slen
+      
+      print(paste("Sequence found for", name, "Getting fasta"))
+      
+      seq.dat <- entrez_fetch(db = "nuccore", id = nuc_search$ids[1], rettype = "fasta")
+      coi_sequence <- c(coi_sequence, seq.dat)
+      
+      print("Fasta added.")
+      seq_count <- seq_count + 1
+    }
+    
+  })
 }
+print("DONE")
 
-cich_ptr_save <- cich_ptr_df
+writeLines(as.character(coi_sequence), "./data/raw/metadata/ptr_out.fasta")
+
+
+# This table will contain all species that had hits for COI
+# Creating a copy for saving
+cich_ptr_save <- valid_tax
 
 # Convert list columns to character strings
 list_cols <- sapply(cich_ptr_save, is.list)
@@ -91,35 +154,63 @@ cich_ptr_save[list_cols] <- lapply(cich_ptr_save[list_cols], function(x) {
   sapply(x, function(y) paste(y, collapse = "; "))
 })
 
-write.csv(cich_ptr_save, "./Cichlid_Ptr.csv", row.names = FALSE)
+write.csv(cich_ptr_save, "./data/raw/Cichlid_Ptr.csv", row.names = FALSE)
 
 
-cich_ptr_df %>% 
-  filter(sapply(accession_num, length) != 0) %>% 
-  count()
-
-# 166 species had results for the Ptr gene
-
+cich_ptr_save %>% 
+  dplyr::filter(accession_num != 'NA') %>% 
+  summarise(count = n())
 
 
 
+#### Now let's look at cytb ####
 
-#### Now let's look at cytb??
+seq_count <- 0
 
-cich_cytb_df <- cich_ptr_df
-
-for (i in 1:nrow(cich_cytb_df)) {
+for (i in 1:nrow(valid_tax)) {
   
-  name <- cich_cytb_df$species_name[i]
-  nuc_search <- entrez_search(db = "nuccore", 
-                              term = paste0(name, "[ORGN] AND cytb gene"), 
-                              retmax = 5)
+  if (seq_count >= 50) {
+    print("50 taxa hits for cytb have been found.")
+    break
+  }
   
-  print(nuc_search$ids)
-  cich_cytb_df$accession_num[i] <- nuc_search$ids[1]
+  name <- valid_tax$species_name[i]
+  
+  tryCatch({
+    
+    nuc_search <- entrez_search(db = "nuccore", 
+                                term = paste0('"', name, '"', '[ORGN] AND cytb[GENE]'), 
+                                retmax = 5)
+    
+    if(length(nuc_search$ids)<1) {
+      valid_tax[i,3:5] <- NA
+    }
+    
+    else {
+      sumout <- entrez_summary(db = "nuccore", id = nuc_search$ids[1])
+      valid_tax[i,3] <- sumout$caption
+      valid_tax[i,4] <- sumout$title
+      valid_tax[i,5] <- sumout$slen
+      
+      print(paste("Sequence found for", name, "Getting fasta"))
+      
+      seq.dat <- entrez_fetch(db = "nuccore", id = nuc_search$ids[1], rettype = "fasta")
+      coi_sequence <- c(coi_sequence, seq.dat)
+      
+      print("Fasta added.")
+      seq_count <- seq_count + 1
+    }
+    
+  })
 }
+print("DONE")
 
-cich_cytb_save <- cich_cytb_df
+writeLines(as.character(coi_sequence), "./data/raw/metadata/cytb_out.fasta")
+
+
+# This table will contain all species that had hits for COI
+# Creating a copy for saving
+cich_cytb_save <- valid_tax
 
 # Convert list columns to character strings
 list_cols <- sapply(cich_cytb_save, is.list)
@@ -127,24 +218,9 @@ cich_cytb_save[list_cols] <- lapply(cich_cytb_save[list_cols], function(x) {
   sapply(x, function(y) paste(y, collapse = "; "))
 })
 
-write.csv(cich_cytb_save, "./Cichlid_cytb.csv", row.names = FALSE)
+write.csv(cich_cytb_save, "./data/raw/Cichlid_cytb.csv", row.names = FALSE)
 
 
-cich_cytb_df %>% 
-  filter(sapply(accession_num, length) != 0) %>% 
-  count()
-
-# 543/1200 species had results for the cytb genes.
-
-
-
-# Now we need to compare all of these tables (Or if you guys have time, just rerun the Ptr and cytb script, but just use a new table that doesn't inlcude NULL accession numnber values)
-# This will give us a workable table to get our fastas
-
-
-
-
-#### Now let's get the fasta files for all these seuquences!
-
-# We need one file per gene, so using the most up to date table ("./Cichlid_cytb.csv"), we can loop through and make a fasta file for each of the three genes.
-
+cich_cytb_save %>% 
+  dplyr::filter(accession_num != 'NA') %>% 
+  summarise(count = n())
