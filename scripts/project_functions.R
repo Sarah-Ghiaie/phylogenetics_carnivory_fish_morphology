59f52a8e8f0e6e5ec0ef33415c816b146fb8079a
Bailey Jannuzzi <137533290+Bailey-JJ@users.noreply.github.com>
2025-12-09 19:55:01 -0700

    Update project_functions.R

--- scripts/project_functions.R
+++ scripts/project_functions.R
@@ -6,13 +6,9 @@ create_ds_optimized <- function(tax_search, list_ids, taxa_target) {
   all_ids <- unique(tax_search$ids) # Just double checking there aren't any repeat names
   n_ids <- length(all_ids)
   
-  print(paste("Processing", n_ids, "taxonomy IDs..."))
-  
-  # Batch processing to reduce API calls
+  # Doing some batch processing to reduce API calls
   batch_size <- 200
   all_records <- list()
-  
-  start_time <- Sys.time()
   
   for (i in seq(1, n_ids, by = batch_size)) {
     end_idx <- min(i + batch_size - 1, n_ids)
@@ -32,23 +28,18 @@ create_ds_optimized <- function(tax_search, list_ids, taxa_target) {
     all_records <- c(all_records, batch_records)
   }
   
-  # Extract species names and filter in vectorized operations
-  print("Extracting species names and filtering...")
-  
-  # Vectorized extraction of scientific names
+  # Getting scientific names
   species_names <- sapply(all_records, function(x) x$scientificname)
   taxids <- names(all_records)
   
-  # Vectorized filtering using %in% (much faster than loop)
+  # Filtering using %in%
   valid_mask <- species_names %in% cichlids_set
   
-  # Keep only valid species
+  # Keeps only valid species
   valid_taxids <- taxids[valid_mask]
   valid_species <- species_names[valid_mask]
   
-  print(paste("Found", sum(valid_mask), "valid species out of", length(species_names), "total"))
-  
-  # Create final dataframe
+  # Create the final dataframe
   n_valid <- length(valid_taxids)
   tax_df <- data.frame(
     taxid = valid_taxids,
@@ -56,91 +47,16 @@ create_ds_optimized <- function(tax_search, list_ids, taxa_target) {
     stringsAsFactors = FALSE
   )
   
-  # Pre-allocate list columns
+  # List columns
   tax_df$accession_num <- vector("list", n_valid)
   tax_df$seq_name <- vector("list", n_valid)
   tax_df$seq_len <- vector("list", n_valid)
-  
-  end_time <- Sys.time()
-  print(paste("Total processing time:", round(difftime(end_time, start_time, units = "mins"), 2), "minutes"))
   
   return(tax_df)
 }
 
 
-# DONT USE THE BELOW FUNCTIONS, JUST RUN THE NORMAL SCRIPTS ON OTHER MODULES
-## Search for gene hits, generate .cv and .fasta files ####
-
-gene_search <- function(valid_tax, gene, outfile_prefix) {
-  
-  valid_tax2 <- valid_tax
-  gene_sequence <- character()
-  seq_count <- 0 # This just counts the number of gene hits
-  
-  for (i in 1:nrow(valid_tax2)) {
-    
-    # Uncomment this section, to run tests on function
-    if (seq_count >= 5) {
-     print("5 taxa hits for COI have been found.")
-     break
-    }
-    
-    name <- valid_tax2$species_name[i]
-    gene_search <- gene
-  
-    
-    tryCatch({
-      
-      nuc_search <- entrez_search(db = "nuccore", 
-                                  term = paste0('"', name, '"', '[ORGN] AND ', gene_search, '[GENE]'), 
-                                  retmax = 5)
-      print(paste0('"', name, '"', '[ORGN] AND ', gene_search, '[GENE]'))
-      
-      
-      if(is.null(nuc_search$ids) || length(nuc_search$ids)<1) { # If no hits appear, add NA to the row
-        print(paste("No sequence found for: ", name))
-        valid_tax2[i,3:5] <- NA
-      }
-      
-      else { # If results appear, add search accession number, gene title, and length.
-        
-        sumout <- entrez_summary(db = "nuccore", id = nuc_search$ids[1])
-        valid_tax2[i,3] <- sumout$caption
-        valid_tax2[i,4] <- sumout$title
-        valid_tax2[i,5] <- sumout$slen
-        
-        print(paste("Sequence found for", name,))
-
-        #seq.dat <- entrez_fetch(db = "nuccore", id = nuc_search$ids[1], rettype = "fasta")
-        #coi_sequence <- c(coi_sequence, seq.dat)
-        
-        #print("Fasta added.")
-        seq_count <- seq_count + 1        
-      }
-      
-    }, error = function(e) {
-      message("Error processing ", name, ": ", e$message)
-      #valid_tax2[i,3:5] <- NA
-      
-    })
-  }
-  print(paste("DONE. Number of hits: ", seq_count))
-
-  
-  # This table will contain all species that had hits for that gene
-  # Convert list columns to character strings
-  list_cols <- sapply(valid_tax2, is.list)
-  valid_tax2[list_cols] <- lapply(valid_tax2[list_cols], function(x) {
-    sapply(x, function(y) paste(y, collapse = "; "))
-  })
-  
-  write.csv(valid_tax2, paste0("./data/raw/metadata/Cichlid_", gene_search, ".csv"), row.names = FALSE)
-  
-  print(".csv results can be found in the ./data/raw/metadata directory.")
-}
-
-
-## Generate .fasta files for each gene using the master .csv ####
+## Generate FASTA files for each gene using the master .csv ####
 
 get_fasta <- function(master_csv, gene, outfile_prefix) {
   master_csv2 <- master_csv
